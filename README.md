ae6da1dc1c21effb675216bbe0d6f017b5df7f8f
Alper Reha Yazgan <alperreha@gmail.com>
2021-12-08 02:20:56 +0300

    web interface added

---
+++ README.md
@@ -0,0 +1,10 @@
+# YTU CDTP Web Control Center  
+
+This project aiming to create a web monitor server for handling all processes coming from Raspberry Pi Highway Sensors. 
+
+# URLs
+GET / - Server Status  
+GET /monitor - Monitor Highway Interface  
+
+
+![monitor-webpage-sample](./utils/monitor-html-sample.jpg)
--- app.py
+++ app.py
@@ -1,21 +1,72 @@
-
-# YTU CDTP WEB CONTROL CENTER
+# YTU CDTP Web Control Server
 # Author: Alper Reha YAZGAN
 # Date: 08.12.2021
 
+#
+#   APP INTERNAL DEPENDENCIES
+#
 import time 
+import os
+import json
+# dotenv
+from dotenv import load_dotenv
+load_dotenv()
+
+# dotenv initial values
+APP_PORT = int(os.getenv("APP_PORT", "5001"))
+APP_BIND = os.getenv("APP_BIND", "0.0.0.0")
+CAMERA_STREAM_URLS = os.getenv("RASP_CAMERA_URLS", "http://127.0.0.1:5000/stream").split(",")
+
+#
+#   FLASK APP IMLEMENTATION
+#
+from flask import Flask,request,render_template
+app = Flask(__name__)
+
+#
+#   SOCKET IO IMPLEMENTATION
+#
+from flask_socketio import SocketIO,send,emit
+socketio = SocketIO(app)
 
 
-def main():
-    print("Hello World")
-    time.sleep(1)
-    print("Hello World")
-    return 0
+# start time
+start_time = time.time()
 
 
-if __name__ == "__main__":
-    main()
-    exit(0)
+# flask main function
+if __name__ == '__main__':
+    print("App initiliazing...")
+    # run from dotenv
+    app.run(app,host=APP_BIND,port=APP_PORT)
+    print("App Server Port: " + APP_PORT)
+    socketio.run(app)
+    print("Socket Server Port: " + APP_PORT)
+    print("App Initialization Finished")
     pass
 
 
+#
+#   FLASK APP ROUTES
+#
+
+# GET / - return running app info
+@app.route('/')
+def system_status():
+    # Status of system as json { "system" : "up", "status" : "ok", "startTime" : "2020-12-08T12:00:00", uptime : "68sec" }
+    uptime = time.time() - start_time
+    uptime = str(round(uptime,2)) + "sec"
+    # return info as json
+    return json.dumps({
+        "service" : "monitor-service",
+        "system" : "up", 
+        "status" : True, 
+        "startTime" : time.strftime("%Y-%m-%dT%H:%M:%S", time.gmtime(start_time)), 
+        "uptime" : uptime
+    })
+
+
+# GET /monitor - return monitor page
+@app.route('/monitor')
+def monitor_page():
+    return render_template("monitor.html", streams = CAMERA_STREAM_URLS)
