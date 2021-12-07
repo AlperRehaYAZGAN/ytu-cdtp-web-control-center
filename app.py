325256ded22d4d620f69c53c3e342c1b4534e1ce
Alper Reha Yazgan <alperreha@gmail.com>
2021-12-08 00:16:46 +0300

    initial commit

---
+++ app.py
@@ -0,0 +1,21 @@
+
+# YTU CDTP WEB CONTROL CENTER
+# Author: Alper Reha YAZGAN
+# Date: 08.12.2021
+
+import time 
+
+
+def main():
+    print("Hello World")
+    time.sleep(1)
+    print("Hello World")
+    return 0
+
+
+if __name__ == "__main__":
+    main()
+    exit(0)
+    pass
+
+
