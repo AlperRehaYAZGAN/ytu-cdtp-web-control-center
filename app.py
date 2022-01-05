338dfe166e005494b89b720dae1e834aaba83c46
Alper Reha Yazgan <alperreha@gmail.com>
2022-01-06 02:05:21 +0300

    ui updated, anomlies added, flask improved

---
+++ app.py
@@ -0,0 +1,384 @@
+#./venv/bin/python.exe
+
+# YTU CDTP - Camera Stream Collector, Anomaly Extractor and Stream Proxy Server
+# Author: Alper Reha YAZGAN
+# Date: 08.12.2021
+
+#
+#   APP INTERNAL DEPENDENCIES
+#
+import os
+import json
+import time 
+# dotenv
+from dotenv import load_dotenv
+load_dotenv( os.path.join(os.getcwd(), '.env'))
+
+# dotenv initial values
+SERVER_APP_BIND = os.getenv("SERVER_APP_BIND", "0.0.0.0")
+SERVER_APP_PORT = int(os.getenv("SERVER_APP_PORT", "5000"))
+
+
+#
+#   Flask Web App Dependencies
+#
+from flask import Flask, render_template, Response
+app = Flask(__name__)
+
+#   Raspberry Pi Camera Dependencies
+#
+import cv2
+# print opencv version
+print("OpenCV version: {}".format(cv2.__version__))
+
+# path to video and cascade model
+car_cascade_src = os.path.join(os.getcwd(), 'haarcascades','cars.xml')
+fire_cascade_src = os.path.join(os.getcwd(), 'haarcascades','fire.xml')
+human_frontface_src = os.path.join(os.getcwd(), 'haarcascades','human_frontface.xml')
+fullbody_src = os.path.join(os.getcwd(), 'haarcascades','fullbody.xml')
+# print paths
+print("Cascade path: {}".format(car_cascade_src))
+print("Cascade path: {}".format(fire_cascade_src))
+print("Cascade path: {}".format(human_frontface_src))
+print("Cascade path: {}".format(fullbody_src))
+# CASCADE CLASSIFIER
+car_cascade = cv2.CascadeClassifier(car_cascade_src)
+fire_cascade = cv2.CascadeClassifier(fire_cascade_src)
+frontface_cascade = cv2.CascadeClassifier(human_frontface_src)
+fullbody_cascade = cv2.CascadeClassifier(fullbody_src)
+
+# camera port from environment variable
+CV2_STREAM_FROM_1 = int(os.getenv("CV2_STREAM_FROM_1",0))
+"""
+# check if CV2_STREAM_FROM_1 is a valid camera port
+if CV2_STREAM_FROM_1.isdigit():
+    # cast to int
+    CV2_STREAM_FROM_1 = int(CV2_STREAM_FROM_1)
+"""
+# print CV2_STREAM_FROM_1
+print("CV2_STREAM_FROM_1:", CV2_STREAM_FROM_1)
+pc_stream_capture_1 = cv2.VideoCapture(CV2_STREAM_FROM_1,cv2.CAP_DSHOW) 
+pc_stream_capture_1.set(cv2.CAP_PROP_FRAME_WIDTH, 480)
+pc_stream_capture_1.set(cv2.CAP_PROP_FRAME_HEIGHT, 360)
+
+# camera port 2 from environment variable
+CV2_STREAM_FROM_2 = int(os.getenv("CV2_STREAM_FROM_2",0))
+"""
+# check if CV2_STREAM_FROM_2 is a valid camera port
+if CV2_STREAM_FROM_2.isdigit():
+    # cast to int
+    CV2_STREAM_FROM_2 = int(CV2_STREAM_FROM_2)
+"""
+# print CV2_STREAM_FROM_2
+print("CV2_STREAM_FROM_2:", CV2_STREAM_FROM_2)
+pc_stream_capture_2 = cv2.VideoCapture(CV2_STREAM_FROM_2,cv2.CAP_DSHOW) 
+pc_stream_capture_2.set(cv2.CAP_PROP_FRAME_WIDTH, 480)
+pc_stream_capture_2.set(cv2.CAP_PROP_FRAME_HEIGHT, 360)
+if not pc_stream_capture_1.isOpened():
+    print("Unable to open camera stream 1")
+    exit()
+    pass
+if not pc_stream_capture_2.isOpened():
+    print("Unable to open camera stream 2")
+    exit()
+    pass
+
+
+from flask_caching import Cache
+
+config = {
+    "DEBUG": True,          # some Flask specific configs
+    "CACHE_TYPE": "SimpleCache",  # Flask-Caching related configs
+    "CACHE_DEFAULT_TIMEOUT": 300
+}
+# tell Flask to use the above defined config
+app.config.from_mapping(config)
+cache = Cache(app)
+cache.init_app(app)
+
+#
+#   Socket Implementation
+#
+from flask_socketio import SocketIO
+sio = SocketIO(app)
+
+# @sio.on('connect')
+def on_connect():
+    print("Client connected")
+sio.on('connect', on_connect)
+
+
+# disconnect
+# @sio.on('disconnect')
+def on_disconnect():
+    print("Client disconnected")
+sio.on('disconnect', on_disconnect)
+
+
+# on error
+@sio.on_error()
+def chat_error_handler(e):
+    print('An error has occurred: ' + str(e))
+
+
+# on anomaly-button-1-clicked
+@sio.on('anomaly-button-1-clicked')
+def anomaly_button_1_clicked(data):
+    sio.emit('anomaly-button-1-clicked', data)
+    pass
+
+# on camera-1-request
+@sio.on('camera-1-request')
+def camera_1_change_cascade(data):
+    changeTo = int(data["to"])
+    if(changeTo != 0 and changeTo > 0 and changeTo < 5):
+        cache.set("req_camera_1", changeTo)
+        sio.emit('camera-1-changed', {"to": int(cache.get("req_camera_1"))})
+        pass
+    pass
+
+# on anomaly-button-2-clicked
+@sio.on('anomaly-button-2-clicked')
+def anomaly_button_2_clicked(data):
+    sio.emit('anomaly-button-2-clicked', data)
+    pass
+
+# on camera-2-request
+@sio.on('camera-2-request')
+def camera_2_change_cascade(data):
+    # log data
+    print("camera-2-request:", data)
+    changeTo = int(data["to"])
+    if(changeTo != 0 and changeTo > 0 and changeTo < 5):
+        cache.set("req_camera_2", changeTo)
+        sio.emit('camera-2-changed', {"to": int(cache.get("req_camera_2"))})
+        pass
+    pass
+
+# on show-display
+@sio.on('show-display')
+def show_display_text(data):
+    displayText = str(data["text"])
+    sio.emit('show-lcd-display', {"text": displayText})
+    pass
+
+# start time
+start_time = time.time()
+# checkpoints for anomaly detection
+
+cache.set('req_camera_1',1)
+cache.set('req_camera_2',1)
+cp_exit_road_1 = time.time()
+cp_exit_road_2 = time.time()
+cp_fire_1 = time.time()
+cp_fire_2 = time.time()
+cp_human_on_road_1 = time.time()
+cp_human_on_road_2 = time.time()
+
+
+# GET / - return running app info
+@app.route('/')
+def system_status():
+    # Status of system as json { "system" : "up", "status" : "ok", "startTime" : "2020-12-08T12:00:00", uptime : "68sec" }
+    uptime = time.time() - start_time
+    uptime = str(round(uptime,2)) + "sec"
+    # return info as json
+    return json.dumps({
+        "service" : "server-anomaly-stream-proxy", 
+        "system" : "up",
+        "status" : True, 
+        "startTime" : time.strftime("%Y-%m-%dT%H:%M:%S", time.gmtime(start_time)), 
+        "uptime" : uptime,
+        "monitorUrl": "/monitor"
+    })
+
+
+"""Web UI for displaying camera streams.""" 
+# GET /monitor - return monitor page to display camera
+@app.route('/monitor') 
+def index(): 
+   return render_template('monitor.html', stream_url_1 = "/anomaly_stream_1", stream_url_2 = "/anomaly_stream_2")
+
+
+
+def proxy_frame_normal_1(): 
+    while True: 
+        try:
+            global cp_fire_1, cp_exit_road_1, cp_human_on_road_1,pc_stream_capture_1,req_camera_1
+            success, frame = pc_stream_capture_1.read() 
+            gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+            req_camera_1 = int(cache.get("req_camera_1"))
+
+
+            if (req_camera_1 == 2):
+                # 2 is fire anomaly
+                fires = fire_cascade.detectMultiScale(gray,1.1,1)
+                # fire finder
+                for (x,y,w,h) in fires:
+                    cv2.rectangle(frame,(x-20,y-20),(x+w+20,y+h+20),(255,0,0),2)
+                    if (time.time() - cp_fire_1) > 3:
+                        sio.emit("anomaly-detected-1", {"type": "ANOMALY_FIRE_EXIST"})
+                        cp_fire_1 = time.time()
+                        pass
+                    pass
+                pass
+            elif (req_camera_1 == 3):
+                # 3 is car exit road anomaly
+                cars = car_cascade.detectMultiScale(gray,1.1,1)
+
+                # highway line borders
+                # left border is 1/4 of the screen width, right border is 3/4 of the screen width. 
+                # Notify with blue line
+                cv2.line(frame, (int(frame.shape[1]/5), 0), (int(frame.shape[1]/5), frame.shape[0]), (255, 0, 0), 2)
+                cv2.line(frame, (int(frame.shape[1]*4/5), 0), (int(frame.shape[1]*4/5), frame.shape[0]), (255, 0, 0), 2)
+                # car finder
+                for (x,y,w,h) in cars:
+                    # check if vehicle exit from highway line (left border or right border)
+                    # if attached to left border, vehicle is leaving from highway red box
+                    # if attached to right border, vehicle is leaving from highway red box
+                    # otherwise, vehicle is staying on highway green box
+                    if (x < int(frame.shape[1]/5)):
+                        # anomaly var
+                        if (time.time() - cp_exit_road_1) > 3:
+                            sio.emit("anomaly-detected-1", {"type": "ANOMALY_CAR_EXIT_ROAD"})
+                            cp_exit_road_1 = time.time()
+                        cv2.rectangle(frame,(x,y),(x+w,y+h),(0,0,255),2)
+                        pass
+                    elif (x > int(frame.shape[1]*4/5)):
+                        # anomaly var
+                        if (time.time() - cp_exit_road_1) > 3:
+                            sio.emit("anomaly-detected-1", {"type": "ANOMALY_CAR_EXIT_ROAD"})
+                            cp_exit_road_1 = time.time()
+                        cv2.rectangle(frame,(x,y),(x+w,y+h),(0,0,255),2)
+                        pass
+                    else:
+                        cv2.rectangle(frame,(x,y),(x+w,y+h),(0,255,0),2)
+                        pass
+                    pass
+                pass
+            elif (req_camera_1 == 4):
+                # 4 is human on road anomaly
+                fullbodies = fullbody_cascade.detectMultiScale(gray,1.1,1)
+                # fullbody finder
+                for (x,y,w,h) in fullbodies:
+                    cv2.rectangle(frame,(x,y),(x+w,y+h),(255,0,0),2)
+                    if (time.time() - cp_human_on_road_1) > 3:
+                        sio.emit('anomaly-detected-1', {'type': 'ANOMALY_HUMAN_ON_ROAD'})
+                        cp_human_on_road_1 = time.time()
+                        pass
+                    pass
+                pass
+            else:
+                # 1 is normal
+                pass
+                    
+
+            ret, buffer = cv2.imencode('.jpg', frame) #, cv2.flip(frame,1))
+            frame = buffer.tobytes()
+            # frame uzerinde sekiller falan ekle
+            yield (b'--frame\r\n' b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+        except Exception as e:
+            print("Break-normal-1:",e)
+            break
+        pass
+    pass
+
+# GET /anomaly_stream_1 - catch stream from camera 1 then return as jpeg image
+@app.route('/anomaly_stream_1') 
+def video_stream_1(): 
+   return Response(proxy_frame_normal_1(), mimetype='multipart/x-mixed-replace; boundary=frame') 
+
+def proxy_frame_normal_2(): 
+    while True: 
+        try:
+            global cp_fire_2, cp_exit_road_2, cp_human_on_road_2,pc_stream_capture_2
+            req_camera_2 = int(cache.get("req_camera_2"))
+            success, frame = pc_stream_capture_2.read() 
+            gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+            if (req_camera_2 == 2):
+                # 2 is fire anomaly
+                fires = fire_cascade.detectMultiScale(frame,1.1,1)
+                # fire finder
+                for (x,y,w,h) in fires:
+                    cv2.rectangle(frame,(x-20,y-20),(x+w+20,y+h+20),(255,0,0),2)
+                    if (time.time() - cp_fire_2) > 3:
+                        sio.emit("anomaly-detected-2", {"type": "ANOMALY_FIRE_EXIST"})
+                        cp_fire_2 = time.time()
+                        pass
+                    pass
+                pass
+            elif (req_camera_2 == 3):
+                # 3 is car exit road anomaly
+                cars = car_cascade.detectMultiScale(gray,1.1,1)
+
+                # highway line borders
+                # left border is 1/4 of the screen width, right border is 3/4 of the screen width. 
+                # Notify with blue line
+                cv2.line(frame, (int(frame.shape[1]/5), 0), (int(frame.shape[1]/5), frame.shape[0]), (255, 0, 0), 2)
+                cv2.line(frame, (int(frame.shape[1]*4/5), 0), (int(frame.shape[1]*4/5), frame.shape[0]), (255, 0, 0), 2)
+                # car finder
+                for (x,y,w,h) in cars:
+                    # check if vehicle exit from highway line (left border or right border)
+                    # if attached to left border, vehicle is leaving from highway red box
+                    # if attached to right border, vehicle is leaving from highway red box
+                    # otherwise, vehicle is staying on highway green box
+                    if (x < int(frame.shape[1]/5) or x > int(frame.shape[1]*4/5)):
+                        # anomaly var
+                        if (time.time() - cp_exit_road_2) > 3:
+                            sio.emit("anomaly-detected-2", {"type": "ANOMALY_CAR_EXIT_ROAD"})
+                            cp_exit_road_2 = time.time()
+                        cv2.rectangle(frame,(x,y),(x+w,y+h),(0,0,255),2)
+                        pass
+                    else:
+                        cv2.rectangle(frame,(x,y),(x+w,y+h),(0,255,0),2)
+                        pass
+                    pass
+                pass
+            elif (req_camera_2 == 4):
+                # 4 is human on road anomaly
+                fullbodies = fullbody_cascade.detectMultiScale(gray,1.1,1)
+                # fullbody finder
+                for (x,y,w,h) in fullbodies:
+                    cv2.rectangle(frame,(x,y),(x+w,y+h),(255,0,0),2)
+                    if (time.time() - cp_human_on_road_2) > 3:
+                        sio.emit('anomaly-detected-2', {'type': 'ANOMALY_HUMAN_ON_ROAD'})
+                        cp_human_on_road_2 = time.time()
+                        pass
+                    pass
+                pass
+            else:
+                # 1 is normal
+                pass
+                    
+
+            ret, buffer = cv2.imencode('.jpg', frame) #, cv2.flip(frame,1))
+            frame = buffer.tobytes()
+            # frame uzerinde sekiller falan ekle
+            yield (b'--frame\r\n' b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+        except Exception as e:
+            print("Break-normal-2:",e)
+            break
+        pass
+    pass
+
+# GET /anomaly_stream_2 - catch stream from camera 2 then return as jpeg image for displaying.
+@app.route('/anomaly_stream_2') 
+def video_stream_2(): 
+   return Response(proxy_frame_normal_2(), mimetype='multipart/x-mixed-replace; boundary=frame') 
+
+
+# flask main function
+if __name__ == '__main__':
+    try:
+        print("PC Server initializing...")
+        # run from dotenv
+        app.run(app,host=SERVER_APP_BIND,port=SERVER_APP_PORT)
+        print("PC Server Port: ", SERVER_APP_PORT)
+        # run socket
+        sio.run(app)
+        print("PC Server Initialization Finished")
+    except Exception as e:
+        print("PC Server Initialization Failed: ", e)
+        pass
+    pass
--- static/js/anomaly.js
+++ static/js/anomaly.js
@@ -10,34 +10,131 @@ socket.on('disconnect', () => {
     console.log("Disconnected from anomaly server");
 });
 
+// lcd btn-display button
+const btnDisplay = document.getElementById('btn-display');
+btnDisplay.onclick = function () {
+    // get text content of input search box where id displaytext
+    const displayText = document.getElementById('displaytext').value;
+    socket.emit('show-display', { text : displayText });
+};
+
+// get buttons
+const camera1tonormal = document.getElementById('camera1tonormal');
+const camera1tofire = document.getElementById('camera1tofire');
+const camera1tocarexit = document.getElementById('camera1tocarexit');
+const camera1tohumanonroad = document.getElementById('camera1tohumanonroad');
+    
+const camera2tonormal = document.getElementById('camera2tonormal');
+const camera2tofire = document.getElementById('camera2tofire');
+const camera2tocarexit = document.getElementById('camera2tocarexit');
+const camera2tohumanonroad = document.getElementById('camera2tohumanonroad');
+
+// camera1status, camera1anomaly, camera1date spans
+const camera1status = document.getElementById('camera1status');
+const camera1anomaly = document.getElementById('camera1anomaly');
+const camera1date = document.getElementById('camera1date');
+
+const camera2status = document.getElementById('camera2status');
+const camera2anomaly = document.getElementById('camera2anomaly');
+const camera2date = document.getElementById('camera2date');
+
+// anomaly-button-1, anomaly-button-1-test, anomaly-button-2, anomaly-button-2-test buttons
+const anomalyButton1 = document.getElementById('anomaly-button-1');
+const anomalyButton1Test = document.getElementById('anomaly-button-1-test');
+const anomalyButton2 = document.getElementById('anomaly-button-2');
+const anomalyButton2Test = document.getElementById('anomaly-button-2-test');
+
+// if camera1tonormal button clicked
+camera1tonormal.onclick = function () {
+    console.log("camera1tonormal button clicked");
+    socket.emit('camera-1-request', { to : 1 });
+};
+
+// if camera1tofire button clicked
+camera1tofire.onclick = function () {
+    console.log("camera1tofire button clicked");
+    socket.emit('camera-1-request', { to : 2 });
+};
+
+// if camera1tocarexit button clicked
+camera1tocarexit.onclick = function () {
+    console.log("camera1tocarexit button clicked");
+    socket.emit('camera-1-request', { to : 3 });
+};
+
+// if camera1tohumanonroad button clicked
+camera1tohumanonroad.onclick = function () {
+    console.log("camera1tohumanonroad button clicked");
+    socket.emit('camera-1-request', { to : 4 });
+};
+
+// if camera2tonormal button clicked
+camera2tonormal.onclick = function () {
+    console.log("camera2tonormal button clicked");
+    socket.emit('camera-2-request', { to : 1 });
+};
+
+// if camera2tofire button clicked
+camera2tofire.onclick = function () {
+    console.log("camera2tofire button clicked");
+    socket.emit('camera-2-request', { to : 2 });
+};
+
+// if camera2tocarexit button clicked
+camera2tocarexit.onclick = function () {
+    console.log("camera2tocarexit button clicked");
+    socket.emit('camera-2-request', { to : 3 });
+};
+
+// if camera2tohumanonroad button clicked
+camera2tohumanonroad.onclick = function () {
+    console.log("camera2tohumanonroad button clicked");
+    socket.emit('camera-2-request', { to : 4 });
+};
+
+// if test1 button clicked
+anomalyButton1Test.onclick = function () {
+    console.log("anomalyButton1Test button clicked");
+    socket.emit('anomaly-btn-1-test', {});
+};
+
+// if test2 button clicked
+anomalyButton2Test.onclick = function () {
+    console.log("anomalyButton2Test button clicked");
+    socket.emit('anomaly-btn-2-test', {});
+};
+
+
 // anomaly-detected-1
 socket.on('anomaly-detected-1', function (data) {
-    // console.log("Anomaly detected: " + data.anomaly);
-    console.log("Anomaly button clicked 1");
-    // set button to red
-    document.getElementById('anomaly-button-1').style.backgroundColor = 'red';
-    // set text to "Anomaly detected"
-    document.getElementById('anomaly-button-1').innerHTML = "Anomaly detected";
+    console.log("Anomaly detected: ", data);
+    // set color to red
+    anomalyButton1.style.backgroundColor = '#ff0000';
+    // set anomaly span content
+    camera1anomaly.innerHTML = data.type;
+    // set current isodate span content
+    camera1date.innerHTML = new Date().toISOString();
     // if the anomaly button is clicked, send a message to the server
-    document.getElementById('anomaly-button-1').onclick = function () {
-        // set color to normal
-        document.getElementById('anomaly-button-1').style.backgroundColor = '#00ff00';
-        socket.emit('anomaly-button-1-clicked', {type : data});
+    anomalyButton1.onclick = function () {
+        socket.emit('anomaly-button-1-clicked', {type : data.type});
     };
+
 });
 
 // anomaly-detected-2
 socket.on('anomaly-detected-2', function (data) {
-    // set button to red
-    document.getElementById('anomaly-button-2').style.backgroundColor = 'red';
-    // set text to "Anomaly detected"
-    document.getElementById('anomaly-button-2').innerHTML = "Anomaly detected";
+    console.log("Anomaly detected: ", data);
+    // set color to red
+    anomalyButton2.style.backgroundColor = '#ff0000';
+    // set anomaly span content
+    camera2anomaly.innerHTML = data.type;
+    // set current isodate span content
+    camera2date.innerHTML = new Date().toISOString();
     // if the anomaly button is clicked, send a message to the server
-    document.getElementById('anomaly-button-2').onclick = function () {
-        // set color to normal
-        document.getElementById('anomaly-button-2').style.backgroundColor = '#00ff00';
-        socket.emit('anomaly-button-2-clicked', { type: data});
+    anomalyButton2.onclick = function () {
+        socket.emit('anomaly-button-2-clicked', {type : data.type});
     };
+
 });
 
 
@@ -56,3 +153,80 @@ document.getElementById('anomaly-button-2').onclick = function () {
     document.getElementById('anomaly-button-2').style.backgroundColor = '#00ff00';
     socket.emit('anomaly-button-2-clicked', { type: "TEST"});
 };
+
+
+// on camera-1-changed
+socket.on('camera-1-changed', function (data) {
+    console.log("Camera 1 changed: ", data);
+    let changedTo = data.to;
+
+
+    // if changed to normal make normal button class btn-primary and others btn-secondary
+    if(changedTo == 1) {
+        camera1tonormal.className = "btn btn-primary";
+        camera1tofire.className = "btn btn-secondary";
+        camera1tocarexit.className = "btn btn-secondary";
+        camera1tohumanonroad.className = "btn btn-secondary";
+        // set status span content
+        camera1status.innerHTML = "LISTENING_NORMAL";
+    } else if(changedTo == 2) {
+        camera1tonormal.className = "btn btn-secondary";
+        camera1tofire.className = "btn btn-primary";
+        camera1tocarexit.className = "btn btn-secondary";
+        camera1tohumanonroad.className = "btn btn-secondary";
+        // set status span content
+        camera1status.innerHTML = "LISTENING_FIRE";
+    } else if(changedTo == 3) {
+        camera1tonormal.className = "btn btn-secondary";
+        camera1tofire.className = "btn btn-secondary";
+        camera1tocarexit.className = "btn btn-primary";
+        camera1tohumanonroad.className = "btn btn-secondary";
+        // set status span content
+        camera1status.innerHTML = "LISTENING_CAR_EXIT";
+    } else if(changedTo == 4) {
+        camera1tonormal.className = "btn btn-secondary";
+        camera1tofire.className = "btn btn-secondary";
+        camera1tocarexit.className = "btn btn-secondary";
+        camera1tohumanonroad.className = "btn btn-primary";
+        // set status span content
+        camera1status.innerHTML = "LISTENING_HUMAN_ON_ROAD";
+    }
+});
+
+
+// on camera-2-changed
+socket.on('camera-2-changed', function (data) {
+    console.log("Camera 2 changed: ", data);
+    let changedTo = data.to;
+
+    // if changed to normal make normal button class btn-primary and others btn-secondary
+    if(changedTo == 1) {
+        camera2tonormal.className = "btn btn-primary";
+        camera2tofire.className = "btn btn-secondary";
+        camera2tocarexit.className = "btn btn-secondary";
+        camera2tohumanonroad.className = "btn btn-secondary";
+        // set status span content
+        camera2status.innerHTML = "LISTENING_NORMAL";
+    } else if(changedTo == 2) {
+        camera2tonormal.className = "btn btn-secondary";
+        camera2tofire.className = "btn btn-primary";
+        camera2tocarexit.className = "btn btn-secondary";
+        camera2tohumanonroad.className = "btn btn-secondary";
+        // set status span content
+        camera2status.innerHTML = "LISTENING_FIRE";
+    } else if(changedTo == 3) {
+        camera2tonormal.className = "btn btn-secondary";
+        camera2tofire.className = "btn btn-secondary";
+        camera2tocarexit.className = "btn btn-primary";
+        camera2tohumanonroad.className = "btn btn-secondary";
+        // set status span content
+        camera2status.innerHTML = "LISTENING_CAR_EXIT";
+    } else if(changedTo == 4) {
+        camera2tonormal.className = "btn btn-secondary";
+        camera2tofire.className = "btn btn-secondary";
+        camera2tocarexit.className = "btn btn-secondary";
+        camera2tohumanonroad.className = "btn btn-primary";
+        // set status span content
+        camera2status.innerHTML = "LISTENING_HUMAN_ON_ROAD";
+    }
+});
